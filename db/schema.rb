a8cea359fca1718b2a97ff8261ce13a174079c5e
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-05 16:55:44 -0500

    added users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210104205054) do
+ActiveRecord::Schema.define(version: 20210105215307) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.string   "name"
