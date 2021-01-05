9372c67d3e1ff523af56c25be930db3d29f24270
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-05 10:39:13 -0500

    added get and post requests

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,3 +1,27 @@
-class WorkoutController < ApplicationController
+class WorkoutsController < ApplicationController
+    get '/workouts' do
+        @workouts = Workout.all
+        erb :'/workouts/index'
+    end
 
+    get '/workouts/new' do
+        erb :'/workouts/new'
+    end
+
+    get '/workouts/:id' do
+        @workout = Workout.find_by_id(params[:id])
+        erb :'workouts/show'
+    end
+
+    post '/workouts' do
+        workout = Workout.new(params[:workout])
+
+        if workout.save
+            redirect '/workouts'
+        else
+            redirect '/workouts/new'
+        end
+    end
+
+    
 end
