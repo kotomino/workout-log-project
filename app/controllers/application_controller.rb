c2013e28f300a2dcd3f99d53b1cb8dc6e7f1e9a7
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-05 20:40:08 -0500

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,28 @@ class ApplicationController < Sinatra::Base
     set :session_secret, ENV["SESSION_SECRET"]
   end
 
+  
+
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def redirect_if_logged_in
+      redirect "/workouts" if logged_in?
+    end
+
+    def redirect_if_not_logged_in
+      redirect "/login" unless logged_in?
+    end
   end
 
 end
