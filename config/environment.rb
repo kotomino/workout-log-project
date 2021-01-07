d1760ebcb4b1b13dcdce38abdc45ed04c8e0f2cb
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-07 15:15:33 -0500

    removed sqlite

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,5 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 require 'dotenv/load'
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-
 require './app/controllers/application_controller'
 require_all 'app'
